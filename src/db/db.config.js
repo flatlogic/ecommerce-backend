1ceee34e4d75956af28ab3af414399a43f51a406
michaeldaineka <michaeldaineka@gmail.com>
2021-02-24 05:24:31 +0300

    init2

--- src/db/db.config.js
+++ src/db/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: "postgres://wyiqlzycpemgin:20fc8b54c9eedd170a0ca447498ee2c67df82b3db36214b0601bba1cfd2572f4@ec2-3-222-11-129.compute-1.amazonaws.com:5432/d5g3e3ke0kepos",
     dialect: "postgres"
   },
   development: {
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const db = require('./db/models');
-const stripe = require("stripe")("sk_test_51HUCprJMc0TzjdrXPHLgWIRXobDv0IBnARG7Urq9P2Vo1sHe8WSbz0njlqG1gRh7G3hMiysLAXIkgK13F6SheDn400agRwXiv7")
+const Stripe = require("stripe");
 
 const authRoutes = require('./routes/auth');
 const fileRoutes = require('./routes/file');
@@ -42,7 +42,7 @@ app.use('/api/products', productsRoutes);
 
 app.use('/api/categories', categoriesRoutes);
 
-app.use('/api/brands', passport.authenticate('jwt', {session: false}), brandsRoutes);
+app.use('/api/brands', brandsRoutes);
 
 app.use('/api/orders', passport.authenticate('jwt', {session: false}), ordersRoutes);
 
@@ -62,39 +62,67 @@ app.get("/images/:entity/:id.:ext", (req, res) => {
   );
 });
 
-if (fs.existsSync(publicDir)) {
-  app.use('/', express.static(publicDir));
+app.post('/payment/session-initiate', async (req, res) => {
+  const {
+    clientReferenceId,
+    customerEmail,
+    lineItem,
+    successUrl,
+    cancelUrl,
+  } = req.body;
 
-  app.get('*', function(request, response) {
-    response.sendFile(
-      path.resolve(publicDir, 'index.html'),
-    );
-  });
-}
+  const stripe = Stripe('sk_test_51HUCprJMc0TzjdrXPHLgWIRXobDv0IBnARG7Urq9P2Vo1sHe8WSbz0njlqG1gRh7G3hMiysLAXIkgK13F6SheDn400agRwXiv7');
 
-app.post("/payment", cors(), async (req, res) => {
-  let { amount, id } = req.body
+  let session;
+
   try {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "USD",
-      description: "Spatula company",
-      payment_method: id,
-      confirm: true
-    })
-    console.log("Payment", payment)
-    res.json({
-      message: "Payment successful",
-      success: true
-    })
+    session = await stripe.checkout.sessions.create({
+      client_reference_id: clientReferenceId,
+      customer_email: customerEmail,
+      payment_method_types: ['card'],
+      line_items: [lineItem],
+      payment_intent_data: {
+        description: `${lineItem.name} ${lineItem.description}`,
+      },
+      success_url: successUrl,
+      cancel_url: cancelUrl,
+    });
   } catch (error) {
-    console.log("Error", error)
-    res.json({
-      message: "Payment failed",
-      success: false
-    })
+    res.status(500).send({ error });
   }
-})
+
+  return res.status(200).send(session);
+});
+
+app.post('/payment/session-complete', async (req, res) => {
+  const stripe = Stripe('sk_test_51HUCprJMc0TzjdrXPHLgWIRXobDv0IBnARG7Urq9P2Vo1sHe8WSbz0njlqG1gRh7G3hMiysLAXIkgK13F6SheDn400agRwXiv7');
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        req.headers['stripe-signature'],
+        'acct_1HUCprJMc0TzjdrX'
+    );
+  } catch (error) {
+    return res.status(400).send(`Webhook Error: ${error.message}`);
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+
+    try {
+      // complete your customer's order
+      // e.g. save the purchased product into your database
+      // take the clientReferenceId to map your customer to a product
+    } catch (error) {
+      return res.status(404).send({ error, session });
+    }
+  }
+
+  return res.status(200).send({ received: true });
+});
 
 const PORT = process.env.PORT || 8080;
 
