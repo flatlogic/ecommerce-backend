6305e62f3fa557bbec1e8b314860e71cad17464a
michaeldaineka <michaeldaineka@gmail.com>
2021-02-24 05:41:58 +0300

    awd

--- src/db/db.config.js
+++ src/db/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   production: {
     use_env_variable: "",
-    dialect: "postgres"
+    dialect: "postgres",
     database: "d5g3e3ke0kepos",
     username: "wyiqlzycpemgin",
     password: "20fc8b54c9eedd170a0ca447498ee2c67df82b3db36214b0601bba1cfd2572f4",
